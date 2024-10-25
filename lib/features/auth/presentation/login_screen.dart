dda243abab42db024f98826ea07d9b49827838c9
Carlos Acosta <cjamcu@gmail.com>
2024-10-25 12:10:31 -0500

    chore : add raidus to containner

--- lib/features/auth/presentation/login_screen.dart
+++ lib/features/auth/presentation/login_screen.dart
@@ -22,38 +22,55 @@ class LoginScreen extends StatelessWidget {
               width: double.infinity,
             ),
             Container(
-              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              transform: Matrix4.translationValues(0.0, -24.0, 0.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 28),
-                  Text(
-                    'Hoola 🖖',
-                    style: Theme.of(context).textTheme.displayMedium,
+                  const SizedBox(height: 52),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 21),
+                    child: Text(
+                      'Hoola 🖖',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
                   ),
-                  const SizedBox(height: 32),
-                  CustomTextField(
-                    label: 'Correo',
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    hintText: 'ejemplo@correo.com',
-                  ),
-                  const SizedBox(height: 32),
-                  CustomTextField(
-                    label: 'Contraseña',
-                    controller: _passwordController,
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                    hintText: '********',
-                  ),
-                  const SizedBox(height: 48),
-                  GradientButton(
-                    text: 'Ingresar',
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushReplacement(CharactersScreen.route());
-                    },
-                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 32),
+                        CustomTextField(
+                          label: 'Correo',
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          hintText: 'ejemplo@correo.com',
+                        ),
+                        const SizedBox(height: 32),
+                        CustomTextField(
+                          label: 'Contraseña',
+                          controller: _passwordController,
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          hintText: '********',
+                        ),
+                        const SizedBox(height: 48),
+                        GradientButton(
+                          text: 'Ingresar',
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacement(CharactersScreen.route());
+                          },
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
